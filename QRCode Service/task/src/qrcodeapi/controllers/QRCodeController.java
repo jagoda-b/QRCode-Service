37fa064e6b86f614dc1ae7682349d5a69bc947f1
jagoda-b <mirreal@gmail.com>
2024-04-14 19:30:05 +0200

    stage 3 finished - In this stage, your task is to add versatility to the QR code service by allowing it to handle different request parameters. This will enable the generation of QR code images with various sizes and formats, as opposed to the same image every time.
                       Clients may need images in different formats and sizes, which requires the modification of the GET /api/qrcode endpoint to accept two request parameters:
    
                       size: this parameter, measured in pixels, should be within a reasonable range. If the value is less than 150 pixels or exceeds 350 pixels, the endpoint should respond with the status code 400 BAD REQUEST, accompanied by a JSON containing the error field and a suitable error message.
    
                       type: the image can be in PNG, JPEG, or GIF format. The endpoint should return the generated image in the requested format, including setting the appropriate Content-Type header. If the parameter value doesn't match these three formats, the endpoint should respond with the status code 400 BAD REQUEST and a JSON containing the error field and a suitable error message.
    
                       Both parameters are mandatory. However, you don't need to account for missing parameters; the default Spring Boot behavior will suffice.

--- QRCode Service/task/src/qrcodeapi/controllers/QRCodeController.java
+++ QRCode Service/task/src/qrcodeapi/controllers/QRCodeController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.imageio.ImageIO;
@@ -11,18 +12,31 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Map;
 
 @RestController
 public class QRCodeController {
 
-    @GetMapping(path = "/api/qrcode", produces = MediaType.IMAGE_PNG_VALUE)
-    public ResponseEntity<byte[]> getQRCode() throws IOException {
-        BufferedImage bufferedImage = generateWhiteImage(250, 250);
+    @GetMapping(path = "/api/qrcode")
+    public ResponseEntity<?> getQRCode(@RequestParam int size, @RequestParam String type) throws IOException {
+        if (size < 150 || size > 350) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(Map.of("error", "Image size must be between 150 and 350 pixels"));
+        }
+
+        if (!type.equals("png") && !type.equals("jpeg") && !type.equals("gif")) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(Map.of("error", "Only png, jpeg and gif image types are supported"));
+        }
+
+        BufferedImage bufferedImage = generateWhiteImage(size, size);
 
         try (var baos = new ByteArrayOutputStream()) {
-            ImageIO.write(bufferedImage, "png", baos);
+            ImageIO.write(bufferedImage, type, baos);
             byte[] bytes = baos.toByteArray();
-            return ResponseEntity.ok().body(bytes);
+            return ResponseEntity.ok().contentType(MediaType.parseMediaType("image/" + type)).body(bytes);
         } catch (IOException e) {
             // handle the IOException
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
