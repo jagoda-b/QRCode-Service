4df025fd12b365da759e848f201105b7a973bb18
jagoda-b <mirreal@gmail.com>
2024-04-14 22:17:10 +0200

    stage 4 finished - Now that you've completed all the necessary preparations, let's start generating QR codes. QR codes, or Quick Response codes, are square-shaped patterns of black squares on a white background. The specific arrangement of these squares encodes information, which could be in the form of text, a URL, geolocation, or other data types. Don't worry; you won't be implementing the data encoding algorithm from scratch. Use the ZXing open-source library for this purpose.

---
+++ QRCode Service/task/src/qrcodeapi/generator/QRGenerator.java
@@ -0,0 +1,18 @@
+package qrcodeapi.generator;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.stereotype.Component;
+
+import java.awt.image.BufferedImage;
+
+@Component
+public class QRGenerator {
+    public BufferedImage generateQRCodeImage(String data, int width, int height) throws WriterException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(data, BarcodeFormat.QR_CODE, width, height);
+        return MatrixToImageWriter.toBufferedImage(bitMatrix);
+    }
+}
