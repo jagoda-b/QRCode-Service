f5033b077a31724925d069239842a945b209bcdc
jagoda-b <mirreal@gmail.com>
2024-04-14 19:21:24 +0200

    stage 2 finished - In this stage, you must create a 250x250 pixel image filled with white color. You can accomplish this using the java.awt.image.BufferedImage class, which allows for image data manipulation.

---
+++ QRCode Service/task/src/qrcodeapi/controllers/HealthController.java
@@ -0,0 +1,15 @@
+package qrcodeapi.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public ResponseEntity<String> checkHealth() {
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
+}
