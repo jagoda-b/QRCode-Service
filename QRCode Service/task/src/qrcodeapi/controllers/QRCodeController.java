6568ac45c344549e6ad01fca267347c440ca6b1d
Ola <mirreal@gmail.com>
2024-04-15 19:40:30 +0200

    stage 5 finished

--- QRCode Service/task/src/qrcodeapi/controllers/QRCodeController.java
+++ QRCode Service/task/src/qrcodeapi/controllers/QRCodeController.java
@@ -1,14 +1,16 @@
 package qrcodeapi.controllers;
 
+import com.google.zxing.WriterException;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import qrcodeapi.generator.QRGenerator;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -16,40 +18,63 @@ import java.util.Map;
 
 @RestController
 public class QRCodeController {
+    private final QRGenerator qrGenerator;
+
+    public QRCodeController(QRGenerator qrGenerator) {
+        this.qrGenerator = qrGenerator;
+    }
 
     @GetMapping(path = "/api/qrcode")
-    public ResponseEntity<?> getQRCode(@RequestParam int size, @RequestParam String type) throws IOException {
+    public ResponseEntity<?> generateQRCode(@RequestParam(required = false, defaultValue = "") String contents,
+                                            @RequestParam(required = false, defaultValue = "250") int size,
+                                            @RequestParam(required = false, defaultValue = "L") String correction,
+                                            @RequestParam(required = false, defaultValue = "png") String type) {
+        if (contents == null || contents.trim().isEmpty()) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(Map.of("error", "Contents cannot be null or blank"));
+        }
         if (size < 150 || size > 350) {
             return ResponseEntity
                     .badRequest()
                     .body(Map.of("error", "Image size must be between 150 and 350 pixels"));
         }
-
+        ErrorCorrectionLevel errorCorrectionLevel;
+        switch (correction) {
+            case "L":
+                errorCorrectionLevel = ErrorCorrectionLevel.L;
+                break;
+            case "M":
+                errorCorrectionLevel = ErrorCorrectionLevel.M;
+                break;
+            case "Q":
+                errorCorrectionLevel = ErrorCorrectionLevel.Q;
+                break;
+            case "H":
+                errorCorrectionLevel = ErrorCorrectionLevel.H;
+                break;
+            default:
+                return ResponseEntity
+                        .badRequest()
+                        .body(Map.of("error", "Permitted error correction levels are L, M, Q, H"));
+        }
         if (!type.equals("png") && !type.equals("jpeg") && !type.equals("gif")) {
             return ResponseEntity
                     .badRequest()
                     .body(Map.of("error", "Only png, jpeg and gif image types are supported"));
         }
 
-        BufferedImage bufferedImage = generateWhiteImage(size, size);
-
         try (var baos = new ByteArrayOutputStream()) {
-            ImageIO.write(bufferedImage, type, baos);
+            BufferedImage qrCodeImage = qrGenerator.generateQRCodeImage(contents, size, size, errorCorrectionLevel);
+            ImageIO.write(qrCodeImage, type, baos);
             byte[] bytes = baos.toByteArray();
             return ResponseEntity.ok().contentType(MediaType.parseMediaType("image/" + type)).body(bytes);
         } catch (IOException e) {
             // handle the IOException
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        } catch (WriterException e) {
+            // handle the WriterException
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
-
-    public BufferedImage generateWhiteImage(int width, int height) {
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        Graphics2D g = image.createGraphics();
-        g.setColor(Color.WHITE);
-        g.fillRect(0, 0, width, height);
-        return image;
-    }
-
-
 }
--- QRCode Service/task/src/qrcodeapi/generator/QRGenerator.java
+++ QRCode Service/task/src/qrcodeapi/generator/QRGenerator.java
@@ -1,18 +1,23 @@
 package qrcodeapi.generator;
+
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import org.springframework.stereotype.Component;
 
 import java.awt.image.BufferedImage;
+import java.util.Map;
 
 @Component
 public class QRGenerator {
-    public BufferedImage generateQRCodeImage(String data, int width, int height) throws WriterException {
+    public BufferedImage generateQRCodeImage(String data, int width, int height, ErrorCorrectionLevel errorCorrectionLevel) throws WriterException {
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        BitMatrix bitMatrix = qrCodeWriter.encode(data, BarcodeFormat.QR_CODE, width, height);
+        Map<EncodeHintType, ?> hints = Map.of(EncodeHintType.ERROR_CORRECTION, errorCorrectionLevel);
+        BitMatrix bitMatrix = qrCodeWriter.encode(data, BarcodeFormat.QR_CODE, width, height, hints);
         return MatrixToImageWriter.toBufferedImage(bitMatrix);
     }
 }
